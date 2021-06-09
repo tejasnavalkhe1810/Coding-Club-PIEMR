e0666388c785e3ef450214c53d7fed5b051677cc
Tejas Navalkhe <navalkhetejas18@gmail.com>
2021-06-09 14:24:39 +0530

    footer updated and new image updated

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,13 @@
 function toggle() {
-  let blur = document.getElementById('blur');
-  blur.classList.toggle('active')
-  let popup = document.getElementById('popup');
-  popup.classList.toggle('active')
+  let blur = document.getElementById('blur'); // main-container
+  blur.classList.toggle('active');
+
+  let footer = document.getElementById('footer'); // hide footer
+  footer.classList.toggle('active');
+
+  let body = document.getElementById('body'); // hide scrolling on body
+  body.classList.toggle('active');
+
+  let popup = document.getElementById('popup'); // registration-form
+  popup.classList.toggle('active');
 }

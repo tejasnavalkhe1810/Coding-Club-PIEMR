c225c4a6ad5e94442831fb19a940b14604415b8e
Tejas Navalkhe <navalkhetejas18@gmail.com>
2021-06-09 15:52:12 +0530

    about-us page complete, responsive left

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,12 +2,20 @@ function toggle() {
   let blur = document.getElementById('blur'); // main-container
   blur.classList.toggle('active');
 
-  let footer = document.getElementById('footer'); // hide footer
-  footer.classList.toggle('active');
-
   let body = document.getElementById('body'); // hide scrolling on body
   body.classList.toggle('active');
 
   let popup = document.getElementById('popup'); // registration-form
   popup.classList.toggle('active');
+}
+
+function about_toggle() {
+  let blur = document.getElementById('blur'); // main-container
+  blur.classList.toggle('active');
+
+  let body = document.getElementById('body'); // hide scrolling on body
+  body.classList.toggle('active');
+
+  let aboutUs = document.getElementById('about');
+  aboutUs.classList.toggle('active');
 }
